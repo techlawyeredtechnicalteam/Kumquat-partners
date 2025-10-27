426639fbd29618820855fdc92da956e676954425
Mike Adeleke <oikeoluwa@gmail.com>
2025-10-27 21:18:21 +0100

    images update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['cdn.sanity.io'],
-    },
+  images: {
+    domains: ["cdn.sanity.io"],
+    unoptimized: false
   }
-  
-  module.exports = nextConfig
-  
+};
+
+module.exports = nextConfig;
